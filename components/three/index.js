ba0428ee657983da85fd59ac05faa1bb0bfff3db
zhengjialux <15879398507@163.com>
2023-09-11 17:57:53 +0800

    尝试three

--- components/three/index.js
+++ components/three/index.js
@@ -1,10 +1,34 @@
 import { useEffect } from 'react'
+import * as THREE from 'three';
 
 // 基础柱状图
 export const Three = ({ data }) => {
-  useEffect(() => {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(1000, 500);
 
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  useEffect(() => {
+    const dom = document.querySelector('.three')
+    dom.appendChild(renderer.domElement);
+    animate();
   }, [])
 
-  return <svg className='three' />
+  const animate = () => {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  }
+
+  return <div className='three' />
 }
--- src/demo/three-page/index.jsx
+++ src/demo/three-page/index.jsx
@@ -6,8 +6,7 @@ export default class ThreePage extends React.Component {
   render() {
     return (
       <div style={{ padding: 24, background: '#f1f3f4', borderRadius: '10px' }}>
-        {/* <Three /> */}
-        three
+        <Three />
       </div>
     )
   }
