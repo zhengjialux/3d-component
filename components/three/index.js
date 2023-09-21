cfd6c69129f5345d54dde6c8795c0b31b9128891
zhengjialux <15879398507@163.com>
2023-09-21 14:31:21 +0800

    update

--- components/three/index.js
+++ components/three/index.js
@@ -3,18 +3,19 @@ import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
 // 基础柱状图
-export const Three = ({ data }) => {
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+export const Three = ({ sceneExtend, camera }) => {
+  const _scene = new THREE.Scene();
+  sceneExtend && sceneExtend(_scene)
+  const _camera = camera || new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(1000, 500);
 
   const geometry = new THREE.BoxGeometry(1, 1, 1);
   const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  _scene.add(cube);
 
-  camera.position.z = 5;
+  _camera.position.z = 5;
 
   useEffect(() => {
     const dom = document.querySelector('.three')
@@ -33,7 +34,7 @@ export const Three = ({ data }) => {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
-    renderer.render(scene, camera);
+    renderer.render(_scene, _camera);
   }
 
   return <div className='three' />
